0008d725bba96fed3f7920913119634abe862162
Icebreaker380 <92775396+Icebreaker380@users.noreply.github.com>
2022-06-02 19:13:38 +1200

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+rightWristX = 0;
+leftWristX = 0;
+difference = 0;
+
 function setup()
 {
     video = createCapture(VIDEO);
@@ -20,10 +24,18 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX = " + leftWristX + ", rightWristX = " + rightWristX + ", difference = " + difference);
     }
 }
 
 function draw()
 {
     background('#6C91C2');
+    document.getElementById("font_size").innerHTML = "Font size of the text will be : " + difference + "px.";
+    textSize(difference);
+    fill('#FFE787');
+    text('Peter', 50, 400);
 }
